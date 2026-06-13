f1a44781a2fd90ccef65baf72c2058ba2478b8c3
Meenakshy Ajai <meenakshyajai69@gmail.com>
2026-06-13 14:14:47 +0530

    added pdf text extraction

--- agents/reader_agent.py
+++ agents/reader_agent.py
@@ -1,4 +1,23 @@
+from pypdf import PdfReader
+
 class ReaderAgent:
 
-    def analyze_paper(self, pdf_path):
-        pass
+    def extract_text(self, pdf_path):
+
+        reader = PdfReader(pdf_path)
+
+        text = ""
+
+        for page in reader.pages:
+
+            text += page.extract_text()
+
+        return text
+
+agent = ReaderAgent()
+
+text = agent.extract_text(
+    "sample_paper.pdf"
+)
+
+print(text[:1000])
